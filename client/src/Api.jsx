03267296cef1b6714299b0f5f98bacdacfb5916a
Achmet <gigsakos@gmail.com>
2026-01-10 22:34:36 +0200

    little changes

--- client/src/Api.jsx
+++ client/src/Api.jsx
@@ -1,4 +1,4 @@
-const api = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://millionixBackend.myrender.eu'
+const api = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : ''
 // const api = 'http://localhost:5000/'
 // const api = 'https://trivialive.myrender.eu/'
 console.log("API URL:", api);
